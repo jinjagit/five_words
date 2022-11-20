c09648b18bcb7a7931297e219490afd69c7a5344
Simon Tharby <simontharby@gmail.com>
2022-11-20 22:09:00 +0000

    Read only suitable words into Vec from word list text file

--- README.md
+++ README.md
@@ -6,6 +6,24 @@ How fast can I find all 5 word lists, where each list does not contain any dupli
 
 Based on question posed in this video: [Someone improved my code by 40,832,277,770%](Someone improved my code by 40,832,277,770%) by Stand-up Maths
 
-Some has done it in 6.5 microseconds (vs. the OP's > 1 month!). I'll be happy with < 1 minute.
+Some has done it in 6.5 microseconds (vs. the OP's > 1 month!). I'll be happy with < 1 minute (using only a single thread).
 
 Uses this word list: https://raw.githubusercontent.com/dwyl/english-words/master/words.txt
+
+## first naive approach
+
+Start by reading lines in using this approach: https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+
+Store each line as a string ('word') in a vec, but filter out 'words' before adding to vec if:
+- contains non-letter character
+- not five characters long
+- contains any duplicate characters
+
+Total runtime: 136 ms
+
+Idea for next step:
+
+Iterate over Vec, stopping at n-1 element.
+Convert first word to vec of chars, and then compare with all other words (also a vecs of chars), checking if combined 2 words contain no duplicate chars.
+- If no duplicates, then save the pair of words in a collection, which includes the index of the words: `Vec<[Vec<char>, index, Vec<char>, index], [...], ...>`
+- do the same for all words, only checking by combining with words later in Vec... this should never repeat a comparison, and will result in all possible word pairs being checked and filtered.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,49 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+use std::time::SystemTime;
+
 fn main() {
-    println!("Hello, world!");
+    let start = SystemTime::now();
+
+    // First, we read the word_list file and add only words we are interested in to a Vec.
+    // Words must be 5 letters, with no other character types, and no duplicate characters.
+
+    let mut word_list: Vec<String> = vec![];
+
+    if let Ok(lines) = read_lines("./words.txt") {
+        for line in lines {
+            if let Ok(ip) = line {
+                let word = ip.to_lowercase();
+
+                // Check word is 5 characters, and all characters are letters
+                if word.chars().count() == 5 && word.to_lowercase().chars().all(|c| matches!(c, 'a'..='z')) {
+                    // Check word does not contain duplicate characters
+                    let mut char_vec: Vec<char> = word.chars().collect();
+                    let char_vec_copy = char_vec.clone();
+                    char_vec.dedup();
+
+                    if char_vec == char_vec_copy {
+                        word_list.push(word);
+                    }
+                }
+            }
+        }
+    }
+
+    let end = SystemTime::now();
+    let duration = end.duration_since(start).unwrap();
+
+    println!("Total runtime: {} ms", duration.as_millis());
+    println!();
+    // println!("word_list: {:?}", word_list);
+    println!("word list length: {:?}", word_list.len());
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
 }
