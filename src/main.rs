26be3c4259d0b0d94e963381b299d4e639477242
Simon Tharby <simontharby@gmail.com>
2022-11-23 00:17:47 +0000

    Update readme

--- src/main.rs
+++ src/main.rs
@@ -35,7 +35,9 @@ fn main() {
     // Find pairs of words with no shared characters
     // 10 s
 
-    let word_pairs: Vec<Vec<Vec<char>>> = find_word_pairs(word_list.clone());
+    let word_pairs: Vec<Vec<char>> = find_word_pairs(word_list.clone());
+
+    let word_quartets: Vec<Vec<char>> = find_word_pairs(word_pairs.clone());
 
     let end = SystemTime::now();
     let duration = end.duration_since(start).unwrap();
@@ -45,9 +47,13 @@ fn main() {
     // print_word_pairs(word_pairs.clone());
     println!("{:?}", word_pairs[13].clone());
     println!();
+
+    println!("{:?}", word_quartets[13].clone());
+    println!();
     println!("Total runtime: {} ms", duration.as_millis());
     println!("word list length: {:?}", word_list.len());
     println!("word pairs length: {:?}", word_pairs.len());
+    println!("word quartets length: {:?}", word_quartets.len());
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -82,23 +88,27 @@ fn vecs_have_no_dups(vec_a: Vec<char>, vec_b: Vec<char>)-> bool {
     true
 }
 
-fn find_word_pairs(word_list: Vec<Vec<char>>) -> Vec<Vec<Vec<char>>> {
-    let mut word_pairs: Vec<Vec<Vec<char>>> = vec![];
+fn find_word_pairs(word_list: Vec<Vec<char>>) -> Vec<Vec<char>> {
+    let mut word_pairs: Vec<Vec<char>> = vec![];
     let word_list_len = word_list.len();
 
     for i in 0..word_list_len {
         if i != word_list_len - 1 {
             for j in (i + 1)..word_list_len {
                 if vecs_have_no_dups(word_list[i].clone(), word_list[j].clone()) {
-                    let word_pair: Vec<Vec<char>> = vec![word_list[i].clone(), word_list[j].clone()];
-
-                    word_pairs.push(word_pair);
+                    word_pairs.push(join_vecs(word_list[i].clone(), word_list[j].clone()));
                 }
             }
         }
     }
 
     word_pairs
+}
+
+fn join_vecs(mut vec_a: Vec<char>, mut vec_b: Vec<char>) -> Vec<char> {
+    vec_a.append(&mut vec_b);
+
+    vec_a
 }
 
 // Debug output
